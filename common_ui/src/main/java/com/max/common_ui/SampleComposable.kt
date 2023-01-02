2c8500d4e13d4e2173e280f48efa404e87e94587
MakrushinM <maksim.makrushin@wolt.com>
2023-01-02 16:34:57 +0200

    Initial commit

---
+++ common_ui/src/main/java/com/max/common_ui/SampleComposable.kt
@@ -0,0 +1,10 @@
+package com.max.common_ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun SampleComposable() {
+    Text("This composable came from common ui library!", color = Color.Cyan)
+}
