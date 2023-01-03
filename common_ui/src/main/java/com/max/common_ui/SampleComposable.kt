1c780a865c38807285c529a9c27ee7b080d39d10
MakrushinM <maksim.makrushin@wolt.com>
2023-01-03 14:01:22 +0200

    This commit came from a separate project! [1]

--- common_ui/src/main/java/com/max/common_ui/SampleComposable.kt
+++ common_ui/src/main/java/com/max/common_ui/SampleComposable.kt
@@ -6,5 +6,5 @@ import androidx.compose.ui.graphics.Color
 
 @Composable
 fun SampleComposable() {
-    Text("This composable came from common ui library!", color = Color.Green)
+    Text("This composable came from common ui library!", color = Color.Green, softWrap = true)
 }
