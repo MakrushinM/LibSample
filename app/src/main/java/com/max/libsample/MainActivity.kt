8ebdc6430b8d497cc690fcf3bdb8df7f0b8880aa
MakrushinM <maksim.makrushin@wolt.com>
2023-01-03 17:37:56 +0200

    Test

--- app/src/main/java/com/max/libsample/MainActivity.kt
+++ app/src/main/java/com/max/libsample/MainActivity.kt
@@ -14,6 +14,7 @@ import com.max.common_ui.SampleComposable
 import com.max.libsample.ui.theme.LibSampleTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,6 +29,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
 
 @Composable
