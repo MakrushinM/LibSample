c18db2631ebb8d5923ba3804e9c9f5b8d3c6d536
MakrushinM <maksim.makrushin@wolt.com>
2023-01-16 10:05:08 +0200

    Temp

--- common_ui/src/main/java/com/max/common_ui/SampleComposable.kt
+++ common_ui/src/main/java/com/max/common_ui/SampleComposable.kt
@@ -6,5 +6,5 @@ import androidx.compose.ui.graphics.Color
 
 @Composable
 fun SampleComposable() {
-    Text("This composable came from common ui library!", color = Color.Blue, softWrap = true, maxLines = 4)
+    Text("This composable came from git submodule!", color = Color.Blue, softWrap = true, maxLines = 4)
 }
